7266e2987813e39dd1ba67f4e2a1f805af61bfd3
SivaSaiRam143 <43082972+SivaSaiRam143@users.noreply.github.com>
2026-06-14 13:08:07 +0530

    Add: merge-two-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-1.py
@@ -0,0 +1,56 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head = None
+        mov_head = None
+        counter=0
+        while list1 or list2:
+            # print(list2.val)
+            # counter+=1
+            # print(f"counter : {counter}")
+            # if counter==20: break
+            if list1 and list2:
+                if list1.val <= list2.val:
+                    if head == None:
+                        head = list1
+                        mov_head = head
+                        list1 = list1.next
+                    else:
+                        mov_head.next = list1
+                        mov_head = mov_head.next
+                        list1 = list1.next
+                elif list2.val <= list1.val:
+                    if head == None:
+                        head = list2
+                        mov_head = head
+                        list2 = list2.next
+                    else:
+                        mov_head.next = list2
+                        mov_head = mov_head.next
+                        list2 = list2.next
+            elif list1:
+                if head == None:
+                    head = list1
+                    mov_head = head
+                    list1 = list1.next
+                else:
+                    mov_head.next = list1
+                    mov_head = mov_head.next
+                    list1 = list1.next    
+            elif list2:
+                if head == None:
+                    head = list2
+                    mov_head = head
+                    list2 = list2.next
+                else:
+                    mov_head.next = list2
+                    mov_head = mov_head.next
+                    list2 = list2.next 
+
+        return head                   
+        
